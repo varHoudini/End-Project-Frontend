448832f0fcc40cd8268623409290c8f31e36530f
Mikailmc <Mikail.cetin1@outlook.com>
2024-01-07 20:07:39 +0100

    Login pagina toegevoegd

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,39 +1,44 @@
 import { Routes } from '@angular/router';
-import { ContactComponent } from './contact/contact.component'; 
-import { HomeComponent } from './home/home.component'; 
-import { ProductenComponent } from './producten/producten.component'; 
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ContactComponent } from './contact/contact.component';
+import { KidsComponent } from './producten/kids/kids.component';
 import { HerenComponent } from './producten/heren/heren.component';
 import { DamesComponent } from './producten/dames/dames.component';
-import { KidsComponent } from './producten/kids/kids.component';
+import { ProductenComponent } from './producten/producten.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        redirectTo: 'home',
-        pathMatch: 'full'
-    },
-    {
-        path: 'home',
-        component: HomeComponent
-    },
-    {
-        path: 'contact',
-        component: ContactComponent
-    },
-    {
-        path: 'producten',
-        component: ProductenComponent
-    },
-    {
-        path: 'kids',
-        component: KidsComponent
-    },
-    {
-        path: 'heren',
-        component: HerenComponent
-    },
-    {
-        path: 'dames',
-        component: DamesComponent
-    }
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'producten',
+    component: ProductenComponent,
+  },
+  {
+    path: 'kids',
+    component: KidsComponent,
+  },
+  {
+    path: 'heren',
+    component: HerenComponent,
+  },
+  {
+    path: 'dames',
+    component: DamesComponent,
+  },
 ];
