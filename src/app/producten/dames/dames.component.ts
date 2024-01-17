bc0d9f36de6e51872d9cec297ad3f6839744afcb
Mikailmc <Mikail.cetin1@outlook.com>
2024-01-17 13:24:49 +0100

    htmlvoor dames endpoint

--- src/app/producten/dames/dames.component.ts
+++ src/app/producten/dames/dames.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-dames',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './dames.component.html',
   styleUrl: './dames.component.css'
 })
-export class DamesComponent {
+export class DamesComponent implements OnInit {
+  productsDames: any
+  url = 'http://localhost:3000/api/productsd'
+  constructor(private http: HttpClient) {}
 
+  ngOnInit(): void {
+    fetch (this.url)
+      .then((response) => response.json())
+      .then((data) => {
+        this.productsDames = data;
+      });
+  }
 }
