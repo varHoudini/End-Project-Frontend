3d9d2e2d6d4091ac7c11f983800ba3f4c4699109
houdini_077 <mohameddenna077@gmail.com>
2024-01-19 11:57:05 +0100

    kids filter

--- src/app/producten/kids/kids.component.ts
+++ src/app/producten/kids/kids.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
@@ -13,13 +13,34 @@ import { CommonModule } from '@angular/common';
 export class KidsComponent implements OnInit {
   productskids: any;
   url = 'http://localhost:3000/api/productsk';
-  constructor(private http: HttpClient) {}
+
+  constructor(private http: HttpClient, private cdr: ChangeDetectorRef) {}
 
   ngOnInit(): void {
-    fetch(this.url)
+    this.fetchProducts();
+  }
+
+  fetchProducts(genderFilter?: string): void {
+    // If there is a gender filter, add it to the URL
+    const apiUrl = genderFilter ? `${this.url}?gender=${genderFilter}` : this.url;
+
+    fetch(apiUrl)
       .then((response) => response.json())
       .then((data) => {
         this.productskids = data;
+
+        // Manually trigger change detection
+        this.cdr.detectChanges();
       });
+  }
+
+  // Add this method to filter boys
+  filterBoys(): void {
+    this.fetchProducts('Kids-B');
+  }
+
+  // Add this method to filter girls
+  filterGirls(): void {
+    this.fetchProducts('Kids-G');
   }
 }
