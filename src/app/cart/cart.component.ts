17108d3c0fc75004bd9a47fada35c949462e28d9
AyKa1903 <aysegulkayar@live.be>
2024-01-19 11:23:18 +0100

    cart typescript

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,16 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css',
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
   cartItem: any;
   removeCartItem(_t5: any) {
     throw new Error('Method not implemented.');
   }
   totalPrice: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    fetch('http://localhost:3000/api/cart')
+      .then((response) => response.json())
+      .then((data) => {
+        this.cartItem = data;
+      });
+  }
 }
