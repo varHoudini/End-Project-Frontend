ad91f297833073cd25da7355a3d72dc67e87a528
Mikailmc <Mikail.cetin1@outlook.com>
2024-01-22 20:22:26 +0100

    snelle fix app.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
-import { ToastrModule } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HttpClientModule, ToastrModule],
+  imports: [CommonModule, RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'endproject';
-
-  
 }
